5682d4ddb134720c811b64a4316c3ba842710e1e
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-08-22 02:40:55 +0900

    ADD : Get Best Seller Books Data & Display

--- app/src/main/java/com/example/bookreview/MainActivity.kt
+++ app/src/main/java/com/example/bookreview/MainActivity.kt
@@ -2,10 +2,74 @@ package com.example.bookreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bookreview.adapter.BookAdapter
+import com.example.bookreview.api.BookService
+import com.example.bookreview.databinding.ActivityMainBinding
+import com.example.bookreview.model.BestSellerDto
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val API_KEY = ""
+    }
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: BookAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBookRecyclerView()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://book.interpark.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val bookService = retrofit.create(BookService::class.java)
+
+        bookService.getBestSeller(API_KEY)
+            .enqueue(object : Callback<BestSellerDto> {
+                override fun onResponse(
+                    call: Call<BestSellerDto>,
+                    response: Response<BestSellerDto>
+                ) {
+                    if (response.isSuccessful.not()) {
+                        Log.d("MainActivity", "isSuccessful not")
+                        return
+                    }
+
+                    Log.d("MainActivity", "isSuccessful")
+
+                    response.body()?.let {
+                        it.books.forEach { book ->
+                            Log.d("MainActivity", book.toString())
+                        }
+
+                        adapter.submitList(it.books)
+                    }
+                }
+
+                override fun onFailure(call: Call<BestSellerDto>, t: Throwable) {
+                    Log.d("MainActivity", "onFailure")
+                }
+
+            })
     }
+
+    private fun initBookRecyclerView() {
+        adapter = BookAdapter()
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+    }
+
 }
